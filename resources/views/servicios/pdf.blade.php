668067b2c0f8bc9f4b28f3f9bbb6396ab5e1526c
rodrigorm93 <33364100+rodrigorm93@users.noreply.github.com>
2017-11-19 00:27:36 -0300

    msjPDF

--- resources/views/servicios/pdf.blade.php
+++ resources/views/servicios/pdf.blade.php
@@ -104,8 +104,8 @@
                     <tbody>
                         <tr>
                             <td class="msg-invoice">
-                                <h4>Message:</h4>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+                                <h4>Mensaje:</h4>
+                                <p>El pago de nuestros servicios por medio de cupones es una facilidad que entrega MUMEFLET para nuestros clientes, sino cumple con el plazo definido en el cupon este ya no sera util y su aviso no se publicara. </p>
                             </td>
                             <!---
                             <td class="right">
